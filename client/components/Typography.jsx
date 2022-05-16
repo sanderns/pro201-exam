427720c9ddbb330c7ba623cb5a7fefcaf11282fa
Sander Nordhagen Svingen <sandersvingen98@gmail.com>
2022-05-17 00:23:32 +0200

    Added Typography component with styling and global styling

---
+++ client/components/Typography.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import styles from "./Typography.module.css";
+
+export function Typography({ element, weight, children }) {
+  // Handles no provided element
+  if (
+    element !== "h1" &&
+    element !== "h2" &&
+    element !== "h3" &&
+    element !== "p1" &&
+    element !== "p2" &&
+    element !== "b1" &&
+    element !== "b2"
+  ) {
+    console.log('Element is not defined! Value set to "p1".');
+    element = "p1";
+  }
+
+  // Handles provided element
+  function getElement() {
+    switch (element) {
+      case "h1":
+        return styles.h1;
+      case "h2":
+        return styles.h2;
+      case "h3":
+        return styles.h3;
+      case "p1":
+        return styles.p1;
+      case "p2":
+        return styles.p2;
+      case "b1":
+        return styles.b1;
+      case "b2":
+        return styles.b2;
+    }
+  }
+
+  const fontElement = getElement();
+
+  // Handles no provided weight
+  if (weight !== "light" && weight !== "regular" && weight !== "bold") {
+    console.log('Weight not defined! Value set to "regular".');
+    weight = "regular";
+  }
+
+  // Handles provided weight
+  function getWeight() {
+    switch (weight) {
+      case "light":
+        return styles.light;
+      case "regular":
+        return styles.regular;
+      case "bold":
+        return styles.bold;
+    }
+  }
+
+  const fontWeight = getWeight();
+
+  // Handles empty children
+  if (children == null || children === "") {
+    console.log('Children not defined! Value set to "undefined".');
+    children = "undefined";
+  }
+
+  // Returns desired type of typography
+  switch (element) {
+    case "h1":
+      return <h1 className={`${fontElement} ${fontWeight}`}>{children}</h1>;
+    case "h2":
+      return <h2 className={`${fontElement} ${fontWeight}`}>{children}</h2>;
+    case "h3":
+      return <h3 className={`${fontElement} ${fontWeight}`}>{children}</h3>;
+    default:
+      return <p className={`${fontElement} ${fontWeight}`}>{children}</p>;
+  }
+}
--- client/index.jsx
+++ client/index.jsx
@@ -1,16 +1,36 @@
 import * as ReactDOM from "react-dom";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import {ListStudents} from "./listStudents";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ListStudents } from "./listStudents";
+import { Typography } from "./components/Typography";
 
+function Frontpage() {
+  const elements = ["h1", "h2", "h3", "p1", "p2", "b1", "b2"];
+  const weights = ["light", "regular", "bold"];
 
-function Application() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path={"/students"} element={<ListStudents />}/>
-            </Routes>
-        </BrowserRouter>
-    )
+  return (
+    <>
+      {elements.map((element) =>
+        weights.map((weight) => (
+          <Typography element={element} weight={weight}>
+            This is a {weight} {element}
+          </Typography>
+        ))
+      )}
+    </>
+  );
 }
 
-ReactDOM.render(<Application/>, document.getElementById("app"));
+function Application() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path={"/"} element={<Frontpage />} />
+          <Route path={"/students"} element={<ListStudents />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+ReactDOM.render(<Application />, document.getElementById("app"));
