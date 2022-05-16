d75785c7980730f9b8f8a6848b559c805f5f7fb3
Sander Nordhagen Svingen <sandersvingen98@gmail.com>
2022-05-16 10:37:42 +0200

    Vb (#2)
    * Connect to database
    
    * test
    
    Co-authored-by: Vithusan1999 <70209043+Vithusan1999@users.noreply.github.com>

--- client/index.jsx
+++ client/index.jsx
@@ -1,7 +1,16 @@
 import * as ReactDOM from "react-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {ListStudents} from "./listStudents";
 
-function App() {
-    return <h1>Hello World</h1>;
+
+function Application() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path={"/students"} element={<ListStudents />}/>
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
-ReactDOM.render(<App/>, document.getElementById("app"))
+ReactDOM.render(<Application/>, document.getElementById("app"));
