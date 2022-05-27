0e927e951bac98f0c5d6ac2f8ad9aeae32f3e09c
Sander <sandersvingen98@gmail.com>
2022-05-27 13:56:50 +0200

    Added DetailedStudentCard component

--- client/src/pages/allStudents.jsx
+++ client/src/pages/allStudents.jsx
@@ -2,22 +2,22 @@ import { fetchJSON } from "../fetchJSON";
 import { StudentCard } from "../components/StudentCard";
 import { useLoading } from "../hooks/useLoading";
 
-function StudentList({
-  student: { name, study, subject, gradeGoal, studyTime, aboutMe },
-}) {
+function StudentList({ student }) {
+  const { name, study, subject, gradeGoal, studyTime, aboutMe } = student;
+
   return (
     <>
       <div>
-        <StudentCard name={name} bio={aboutMe} />
+        <StudentCard student={student} />
       </div>
-      <div>
-        <h2>Name: {name}</h2>
-        <h3>Study: {study}</h3>
-        <h3>Subjects: {subject}</h3>
-        <h3>Grade goal: {gradeGoal}</h3>
-        <h3>Study time: {studyTime}</h3>
-        <h3>About me: {aboutMe}</h3>
-      </div>
+      {/*<div>*/}
+      {/*  <h2>Name: {name}</h2>*/}
+      {/*  <h3>Study: {study}</h3>*/}
+      {/*  <h3>Subjects: {subject}</h3>*/}
+      {/*  <h3>Grade goal: {gradeGoal}</h3>*/}
+      {/*  <h3>Study time: {studyTime}</h3>*/}
+      {/*  <h3>About me: {aboutMe}</h3>*/}
+      {/*</div>*/}
     </>
   );
 }
@@ -42,7 +42,6 @@ export function AllStudents() {
   return (
     <div>
       <h1>List of students</h1>
-
       {data.map((student) => (
         <StudentList key={student.name} student={student} />
       ))}
