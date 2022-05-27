2fd7ec17cfcfbee3b746eb2fa7e33dc975e6c4a5
Sander Nordhagen Svingen <sandersvingen98@gmail.com>
2022-05-27 12:33:37 +0200

    Fixed stuff (#14)

--- client/src/pages/allStudents.jsx
+++ client/src/pages/allStudents.jsx
@@ -1,3 +1,51 @@
+import { fetchJSON } from "../fetchJSON";
+import { StudentCard } from "../components/StudentCard";
+import { useLoading } from "../hooks/useLoading";
+
+function StudentList({
+  student: { name, study, subject, gradeGoal, studyTime, aboutMe },
+}) {
+  return (
+    <>
+      <div>
+        <StudentCard name={name} bio={aboutMe} />
+      </div>
+      <div>
+        <h2>Name: {name}</h2>
+        <h3>Study: {study}</h3>
+        <h3>Subjects: {subject}</h3>
+        <h3>Grade goal: {gradeGoal}</h3>
+        <h3>Study time: {studyTime}</h3>
+        <h3>About me: {aboutMe}</h3>
+      </div>
+    </>
+  );
+}
+
 export function AllStudents() {
-    return null;
+  const { loading, error, data } = useLoading(async () =>
+    fetchJSON("/api/students")
+  );
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return (
+      <div>
+        <h1>Error</h1>
+        <div>{error.toString()}</div>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h1>List of students</h1>
+
+      {data.map((student) => (
+        <StudentList key={student.name} student={student} />
+      ))}
+    </div>
+  );
 }
