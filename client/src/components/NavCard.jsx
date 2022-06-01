939bd84e589fc30057b50a6c7bd77c5086ebe8ab
Sander Nordhagen Svingen <sandersvingen98@gmail.com>
2022-06-01 23:22:24 +0200

    Changed NavCards from href to buttons

--- client/src/components/NavCard.jsx
+++ client/src/components/NavCard.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { Typography } from "./Typography";
+import { useNavigate } from "react-router-dom";
 
 export function NavCard({ href, element, children }) {
+  const navigate = useNavigate();
+
   return (
-    <a
-      href={href}
+    <button
+      onClick={() => navigate(href)}
       className={`rounded p-10 bg-white drop-shadow rounded flex justify-center items-center`}
     >
       <Typography element={element} weight={"regular"}>
         {children}
       </Typography>
-    </a>
+    </button>
   );
 }
