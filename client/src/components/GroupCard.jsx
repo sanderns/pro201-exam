ecea4e3b1c8a3ad3d0643d861b6984b877d3b4d8
Sander Nordhagen Svingen <sandersvingen98@gmail.com>
2022-06-02 18:56:02 +0200

    Added icons for group members in group cards

--- client/src/components/GroupCard.jsx
+++ client/src/components/GroupCard.jsx
@@ -5,7 +5,7 @@ import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined
 
 function GroupMemberCircle() {
   return (
-    <div className="bg-gradient-to-r from-gradient-left to-gradient-right w-10 h-10 rounded-full flex justify-center items-center text-white shadow-lg">
+    <div className="bg-gradient-to-r from-gradient-left to-gradient-right w-10 h-10 rounded-full flex justify-center items-center text-white shadow-md">
       <PersonOutlineOutlinedIcon />
     </div>
   );
@@ -27,8 +27,20 @@ export function GroupCard({ group, onClick }) {
       <Typography element={"p2"} weight={"regular"}>
         {aboutUs}
       </Typography>
-      <div className="flex">
-        <GroupMemberCircle />
+      <div className="relative h-12">
+        {/* Functionality to map users in group to show the four or five most recently active users */}
+        <div className="absolute left-24">
+          <GroupMemberCircle />
+        </div>
+        <div className="absolute left-16">
+          <GroupMemberCircle />
+        </div>
+        <div className="absolute left-8">
+          <GroupMemberCircle />
+        </div>
+        <div className="absolute left-0">
+          <GroupMemberCircle />
+        </div>
       </div>
       <Button type={"gradient"} onClick={() => onClick(group)}>
         View more
