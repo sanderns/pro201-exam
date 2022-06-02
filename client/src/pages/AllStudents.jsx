aa24bc8ec098a6edbd7d477738b5162c4f0a786a
Vithusan1999 <70209043+Vithusan1999@users.noreply.github.com>
2022-06-02 14:40:14 +0200

    Search bar

--- client/src/pages/AllStudents.jsx
+++ client/src/pages/AllStudents.jsx
@@ -4,8 +4,30 @@ import { useLoading } from "../hooks/useLoading";
 import React, { useState } from "react";
 import { DetailedStudentCard } from "../components/DetailedStudentCard";
 import { ModalWrapper } from "../components/wrappers/ModalWrapper";
+import {SearchBar} from "../components/ui/SearchBar";
+import {Button} from "../components/ui/Button";
+import students from "tailwindcss/peers";
+
+function search(input) {
+
+  const [searchTerm, setSearchTerm] = useState("");
+
+  {
+    students.filter((student)=>{
+    if (searchTerm == "") {
+      return student
+    } else if (StudentCard.toLowerCase().includes(searchTerm.toLowerCase())){
+
+    }
+
+    }).map((student, index) => (
+        <StudentCard key={index} student={student}/>
+    ))
+}
+}
 
 export function AllStudents() {
+  const [input, setInput] = useState(undefined);
   const [selectedStudent, setSelectedStudent] = useState(undefined);
   const [showModal, setShowModal] = useState(false);
   const {
@@ -34,6 +56,12 @@ export function AllStudents() {
 
   return (
     <div className="relative">
+      <form onSubmit={() =>search(input)} className="p-5">
+        <SearchBar onChange={setInput} />
+        {input}
+        <Button type={"flat"}>Submit</Button>
+      </form>
+      );
       {students.map((student, index) => (
         <StudentCard key={index} student={student} onClick={handleClick} />
       ))}
